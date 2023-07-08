923a3e5b335656a045614188d87cab4f52187982
AkshayaPatil5 <akshayapatil1987@gmail.com>
2023-07-08 14:52:22 +0530

    Inputoverthebox

--- app2.js
+++ app2.js
@@ -6,14 +6,24 @@ const server=http.createServer((req,res)=>{
     const url = req.url; 
     const method= req.method;
     
-
+    
+    
     if(url === '/'){
+        fs.readFile('message.txt',{encoding:'utf-8'},(err,data) => {
+            if(err){
+                console.log(err)
+            }
+        console.log("data from file" + data)
         res.write('<html>');
         res.write('<head><title>FirstApp</title></head>');
+        res.write(`<body>${data}</body>`);
         res.write('<body><form action="/message" method ="POST" ><input type = "text" name = "message"><button type = "submit">Send</button><form></body>')
         res.write('</html>');
-        return res.end()
+        return res.end();
+        })
     }
+    
+
     if(url === '/message' && method === 'POST'){
         const body=[]
         req.on('data',(chunk)=>{
