54bb824e4ddbadf883174ab84eca940d476095ac
AkshayaPatil5 <akshayapatil1987@gmail.com>
2023-07-08 20:09:33 +0530

    routes

--- app2.js
+++ app2.js
@@ -1,51 +1,7 @@
 const http = require('http');
-const fs = require('fs');
 
-const server=http.createServer((req,res)=>{
+const routes = require('./routes');
 
-    const url = req.url; 
-    const method= req.method;
-    
-    
-    
-    if(url === '/'){
-        fs.readFile('message.txt',{encoding:'utf-8'},(err,data) => {
-            if(err){
-                console.log(err)
-            }
-        console.log("data from file" + data)
-        res.write('<html>');
-        res.write('<head><title>FirstApp</title></head>');
-        res.write(`<body>${data}</body>`);
-        res.write('<body><form action="/message" method ="POST" ><input type = "text" name = "message"><button type = "submit">Send</button><form></body>')
-        res.write('</html>');
-        return res.end();
-        })
-    }
-    
-
-    if(url === '/message' && method === 'POST'){
-        const body=[]
-        req.on('data',(chunk)=>{
-            console.log(chunk)
-            body.push(chunk)
-
-        });
-        return req.on('end',()=>{
-            const parsedBody= Buffer.concat(body).toString();
-            const message= parsedBody.split('=')[1];
-            
-            fs.writeFile('message.txt', message, err =>{
-                res.statusCode = 302;
-                res.setHeader('Location','/');
-                return res.end()
-
-            });
-            
-
-        });
-
-    }
-
-})
-server.listen(3000)
+const server = http.createServer(routes);
+   
+server.listen(3000);
