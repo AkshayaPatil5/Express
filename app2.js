68c217cae89af751cf678f401c1041f1103dc5f8
AkshayaPatil5 <akshayapatil1987@gmail.com>
2023-07-11 10:49:57 +0530

    expressjs

--- app2.js
+++ app2.js
@@ -1,8 +1,17 @@
 const http = require('http');
+const express = require('express');
+const app= express();
 
-const routes = require('./routes');
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+app.use((req,res,next)=>{
+    console.log("in the middleware");
+    next();
+})
+app.use((req,res,next)=>{
+    console.log("in the another middleware");
+    res.send('<h1>Hello from Express</h1>')
+})
+
+
    
 
-server.listen(3000);
+app.listen(3000); 
